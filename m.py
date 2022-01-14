dabeeb0193b503710a76e450e8be9e294d76dbb9
bitsofcotton <22259589+bitsofcotton@users.noreply.github.com>
2022-01-14 11:22:06 +0900

    ok.

--- m.py
+++ m.py
@@ -1,6 +1,5 @@
 import sys
 import numpy as np
-from scipy.linalg import svdvals
 
 # we suppose symmetric matrix.
 m = []
@@ -8,5 +7,8 @@ for y in range(0, int(sys.argv[1])):
   m.append([])
   for x in range(0, int(sys.argv[1])):
     m[- 1].append(eval(sys.argv[2]))
-print(svdvals(np.matrix(m))[- 1])
+m = np.matrix(m)
+m = m * m.transpose()
+q, r = np.linalg.qr(m * m.transpose())
+print(np.sqrt(abs(r[int(sys.argv[1]) - 1, int(sys.argv[1]) - 1])))
 
